fe8466bd1de70fb5284bebfa3fbea9b87f4694b8
rifkym <rratnawm@gmail.com>
2017-05-01 23:55:52 +0700

    Update ControllerPetugasLihatRute.java

--- Sistem Informasi Data Penjualan Tiket/src/Controller/ControllerPetugasLihatRute.java
+++ Sistem Informasi Data Penjualan Tiket/src/Controller/ControllerPetugasLihatRute.java
@@ -1,14 +1,39 @@
-/*
+/*/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package Controller;
 
+import java.awt.event.ActionListener;
+import Model.Aplikasi;
+import Model.*;
+import View.*;
+import View.HalamanPetugas;
+import java.awt.event.ActionEvent;
+
 /**
  *
- * @author USER
+ * @author rifky
  */
-public class ControllerPetugasLihatRute {
-    
+public class ControllerPetugasLihatRute implements ActionListener {
+    Aplikasi model;
+    PetugasLihatRute view;
+
+    public ControllerPetugasLihatRute(Aplikasi model) {
+        this.model = model;
+        view = new PetugasLihatRute();
+        view.setVisible(true);
+        view.addListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        
+        if (source.equals(view.getBtnOk())) {
+            ControllerHalamanPetugas hp = new ControllerHalamanPetugas();
+            view.dispose();
+        }
+    }
 }
