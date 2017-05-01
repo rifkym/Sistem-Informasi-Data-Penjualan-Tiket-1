00f3558f0e2efa55b7c93d293f37037fd383755e
rifkym <rratnawm@gmail.com>
2017-05-01 23:53:29 +0700

    Update ControllerLoginPenumpang.java

--- Sistem Informasi Data Penjualan Tiket/src/Controller/ControllerLoginPenumpang.java
+++ Sistem Informasi Data Penjualan Tiket/src/Controller/ControllerLoginPenumpang.java
@@ -1,14 +1,58 @@
-/*
+/*/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package Controller;
 
+import java.awt.event.ActionListener;
+import View.LoginPenumpang;
+import Model.Aplikasi;
+import Model.Database;
+import Model.*;
+import Model.Penumpang;
+import java.awt.event.ActionEvent;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author USER
+ * @author rifky
  */
-public class ControllerLoginPenumpang {
+public class ControllerLoginPenumpang implements ActionListener{
+    Aplikasi model;
+    LoginPenumpang view;
+    Penumpang pen;
+    
+    public ControllerLoginPenumpang(Aplikasi model) {
+        this.model = model;
+        view = new LoginPenumpang();
+        view.setVisible(true);
+        view.addListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       Object source = e.getSource();
+       
+       if(source.equals(view.getBtnLogin())) {
+           String usnm = view.getUsername();
+           String pass = view.getPassword();
+           try {
+               pen = model.getPenumpang(usnm, pass);
+           } catch (SQLException ex) {
+               ex.printStackTrace();
+           }
+           if(pen != null) {
+               
+           } else {
+               JOptionPane.showMessageDialog(view, "Username atau Password salah");
+               view.setUsername(usnm);
+               view.setPassword(pass);
+           }
+       }
+    }
     
 }
