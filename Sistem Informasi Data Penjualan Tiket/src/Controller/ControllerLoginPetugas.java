a33ac66cd51d0cd9bcfe81a866e30858c52ad499
rifkym <rratnawm@gmail.com>
2017-05-01 23:55:02 +0700

    Update ControllerLoginPetugas.java

--- Sistem Informasi Data Penjualan Tiket/src/Controller/ControllerLoginPetugas.java
+++ Sistem Informasi Data Penjualan Tiket/src/Controller/ControllerLoginPetugas.java
@@ -5,10 +5,50 @@
  */
 package Controller;
 
+import java.awt.event.ActionListener;
+import View.LoginPetugas;
+import Model.Petugas;
+import Model.Aplikasi;
+import Model.Database;
+import java.awt.event.ActionEvent;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
 /**
  *
- * @author USER
+ * @author rifky
  */
-public class ControllerLoginPetugas {
+public class ControllerLoginPetugas implements ActionListener {
+    LoginPetugas view;
+    Aplikasi model;
+    Petugas pet;
     
+    public ControllerLoginPetugas(Aplikasi model) {
+        this.model = model;
+        view = new LoginPetugas();
+        view.setVisible(true);
+        view.addListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        
+        if(source.equals(view.getBtnLogin())) {
+            String usrnm = view.getUsername();
+            String pass = view.getPassword();
+            pet = model.getPetugas(usrnm, pass);
+            if(pet != null) {
+                new ControllerHalamanPetugas();
+                view.dispose();
+            } else {
+                JOptionPane.showMessageDialog(view, "Username atau Password Salah!");
+                view.setUsername("");
+                view.setPassword("");
+            }
+        } 
+    }
 }
